081e8c02cdd53410752b74d2e412b97571de9261
edwinfang01 <edwinfang05@gmail.com>
2025-01-25 20:50:18 -0400

    Add files via upload

---
+++ ano bisiesto/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello, World!");
+
+void bisiesto(int leapYear){
+    if ( (leapYear % 4 == 0 && leapYear % 100 != 0) || (leapYear % 400 == 0) ) {
+        Console.WriteLine("es bisiesto");
+        return;
+    }
+    Console.WriteLine("No es bisiesto");
+}
+
+bisiesto(2016);
